99a03849920f58f0cce76f3f3313ad6f21bd0b25
renato Souza <renato.souza@gmail.com>
2022-11-09 20:51:41 -0500

    Add - security configuration, - registration controller, service... - UserApp entity, service, repository,...

---
+++ src/main/java/com/rs/springsecuritydemo/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.rs.springsecuritydemo.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/rs/springsecuritydemo/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.rs.springsecuritydemo.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "Works";
+    }
+}
